fa4a30a4904fbdbce0e07d55ad52aaadea30f1da
Sahil-tarento <140611066+Sahil-tarento@users.noreply.github.com>
2025-01-17 19:02:25 +0530

    Update the log

--- searchIndex-platform/module/searchindex-elasticsearch/src/main/java/org/sunbird/searchindex/elasticsearch/ElasticSearchUtil.java
+++ searchIndex-platform/module/searchindex-elasticsearch/src/main/java/org/sunbird/searchindex/elasticsearch/ElasticSearchUtil.java
@@ -276,11 +276,7 @@ public class ElasticSearchUtil {
 							.source((Map<String, Object>) jsonObjects.get(key)));
 					if (count % BATCH_SIZE == 0 || (count % BATCH_SIZE < BATCH_SIZE && count == jsonObjects.size())) {
 						BulkResponse bulkResponse = client.bulk(request);
-						BulkItemResponse[] responses = bulkResponse.getItems(); 
-						for (BulkItemResponse response : responses) 
-							{ if (response.isFailed()) 
-							{ System.out.println("Failed Document ID: " + response.getId()); 
-							System.out.println("Failure Message: " + response.getFailureMessage()); } else { System.out.println("Successfully Indexed Document ID: " + response.getId()); } } } else { System.out.println("All documents in this batch indexed successfully."); }
+						System.out.println("Successfully Indexed Document ID");
 						if (bulkResponse.hasFailures()) {
 							TelemetryManager
 									.log("Failures in Elasticsearch bulkIndex : " + bulkResponse.buildFailureMessage());
