411ce4c27830c936bd03b8c834cb2f30ef798b0c
jayaprakashnarayanaswamy <jayaprakash.n@tarento.com>
2025-11-13 14:53:27 +0530

    "debug audit history empty response"

--- platform-modules/manager/src/main/java/org/sunbird/taxonomy/mgr/impl/AuditHistoryManager.java
+++ platform-modules/manager/src/main/java/org/sunbird/taxonomy/mgr/impl/AuditHistoryManager.java
@@ -139,7 +139,7 @@ public class AuditHistoryManager implements IAuditHistoryManager {
 	public Response getAuditHistoryById(String graphId, String objectId, String startTime, String endTime,
 			String versionId) {
 		Request request = new Request();
-		TelemetryManager.log("Checking if received parameters are empty or not" + graphId + objectId);
+		TelemetryManager.info("Checking if received parameters are empty or not" + graphId + objectId);
 		if (StringUtils.isNotBlank(graphId) && StringUtils.isNotBlank(objectId)) {
 			request.put(CommonDACParams.graph_id.name(), graphId);
 			request.put(CommonDACParams.object_id.name(), objectId);
@@ -147,14 +147,14 @@ public class AuditHistoryManager implements IAuditHistoryManager {
 		request.put(CommonDACParams.start_date.name(), startTime);
 		request.put(CommonDACParams.end_date.name(), endTime);
 
-		TelemetryManager.log("Sending request to auditHistoryEsService" , request.getRequest());
+		TelemetryManager.info("Sending request to auditHistoryEsService" , request.getRequest());
 		Response response = null;
 		try {
 			response = auditHistoryEsService.getAuditHistoryLogByObjectId(request, versionId);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		TelemetryManager.log("Response received from the auditHistoryEsService as a result" , response.getResult());
+		TelemetryManager.info("Response received from the auditHistoryEsService as a result" , response.getResult());
 		return response;
 	}
 
